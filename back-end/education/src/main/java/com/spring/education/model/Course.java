2cfa29cce0103a1c70c278941a343aaa730b7484
EslamKhder <eslamkhder81@gmail.com>
2020-10-31 22:34:01 +0200

    put annotation Closes #2

--- back-end/education/src/main/java/com/spring/education/model/Course.java
+++ back-end/education/src/main/java/com/spring/education/model/Course.java
@@ -3,17 +3,23 @@ package com.spring.education.model;
 
 import com.spring.education.model.enums.Content;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    private String courseName;
-
-    private String coursePrice;
-
+    @Enumerated(EnumType.ORDINAL)
     private Content content;
+
+    @Column(name = "course_price")
+    private String coursePrice;
 
     private List<Master> masters;
 }
--- back-end/education/src/main/java/com/spring/education/model/Master.java
+++ back-end/education/src/main/java/com/spring/education/model/Master.java
@@ -1,29 +1,46 @@
 package com.spring.education.model;
 
 import com.spring.education.model.enums.Gender;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity(name = "teacher")
 public class Master {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "first_name")
     private String firstname;
 
+    @Column(name = "last_name")
     private String lastname;
 
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @Column(name = "age")
     private String age;
 
+    @Column(name = "phone_number")
     private String phone;
 
+    @Column(name = "address")
     private String address;
 
+    @Temporal(TemporalType.DATE)
     private Date date;
 
+    @CreationTimestamp
+    @Column(name = "date_create")
     private Date dateCreated;
 
+    @UpdateTimestamp
     private Date lastUpdated;
 
     private Course course;
