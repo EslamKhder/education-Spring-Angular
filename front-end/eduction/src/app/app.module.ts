56926d86ab74d228da3924b679ac94e58e18676a
EslamKhder <eslamkhder81@gmail.com>
2020-11-09 03:25:07 +0200

    create components (header - footer - profile - courses - my courses - exam) Closes #12

--- front-end/eduction/src/app/app.module.ts
+++ front-end/eduction/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './componats/profile/profile.component';
+import { CoursesComponent } from './componats/courses/courses.component';
+import { MyCoursesComponent } from './componats/my-courses/my-courses.component';
+import { ExamComponent } from './componats/exam/exam.component';
+import { HeaderComponent } from './componats/header/header.component';
+import { FooterComponent } from './componats/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    CoursesComponent,
+    MyCoursesComponent,
+    ExamComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
